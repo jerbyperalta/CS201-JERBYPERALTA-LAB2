7a2fab76046f8436f3f3052dbcd03f18f4686cf6
jerbyperalta <147475724+jerbyperalta@users.noreply.github.com>
2023-10-10 14:17:12 +0800

    Add files via upload

---
+++ Activity2.js
@@ -0,0 +1,11 @@
+let firstFavNum = 25;
+let secondFavNum = 19;
+
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum != secondFavNum);
